00be6026ebfe8e086276694e1297000b03658805
Gustav Gränsbo <gusgr644@student.liu.se>
2020-03-14 19:24:27 +0100

    Handle errors reading file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
+    
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
@@ -12,10 +13,17 @@ fn main() {
 
     println!("Searching for '{}' in file '{}'", config.query, config.filename);
 
-    let file_content = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let file_content = fs::read_to_string(config.filename)?;
 
     println!("File content: {}", file_content);
+    Ok(())
 }
 
 struct Config {
