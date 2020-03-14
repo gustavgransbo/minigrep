4af547250715fcaae11e2f9d72afa31e79703434
Gustav Gränsbo <gusgr644@student.liu.se>
2020-03-14 18:34:39 +0100

    Read and print specified file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,4 +8,9 @@ fn main() {
     let filename = &args[2];
 
     println!("Searching for '{}' in file '{}'", query, filename);
+
+    let file_content = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("File content: {}", file_content);
 }
