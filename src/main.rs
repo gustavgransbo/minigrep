876d18edcbc0fb714e630e5887abb0401b3d845a
Gustav Gränsbo <gusgr644@student.liu.se>
2020-03-14 18:51:43 +0100

    Contain arguments in Config class

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,26 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
 
-    println!("Searching for '{}' in file '{}'", query, filename);
+    println!("Searching for '{}' in file '{}'", config.query, config.filename);
 
-    let file_content = fs::read_to_string(filename)
+    let file_content = fs::read_to_string(config.filename)
         .expect("Something went wrong reading the file");
 
     println!("File content: {}", file_content);
+}
+
+struct Config {
+    query: String,
+    filename: String
+}
+
+impl Config {
+    fn new(args : &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        
+        Config {query, filename}
+    }
 }
