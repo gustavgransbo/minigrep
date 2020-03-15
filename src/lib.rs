bcb6460fd189f6d89b49fe23b6ee7762dc03838b
Gustav Gränsbo <gusgr644@student.liu.se>
2020-03-15 11:22:34 +0100

    Remove print statements

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,5 @@ pub mod config;
 
 pub fn run(config: config::Config) -> Result<(), Box<dyn Error>> {
     let file_content = fs::read_to_string(config.filename)?;
-    println!("File content: {}", file_content);
     Ok(())
 }
