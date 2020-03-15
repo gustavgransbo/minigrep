cf13ca4fa95741ec57120b744726586eafc9a8a2
Gustav Gränsbo <gusgr644@student.liu.se>
2020-03-15 11:03:07 +0100

    Add tests of Config constructor

--- src/lib.rs
+++ src/lib.rs
@@ -24,3 +24,41 @@ impl Config {
         Ok(Config {query, filename})
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_config_too_few_arguments() {
+        let args = [String::from("foo"), String::from("bar")];
+        assert!(Config::new(&args).is_err());
+    }
+
+    #[test]
+    fn new_config_ok() {
+        let args = [String::from("foo"), String::from("bar"), String::from("baz")];
+        let result = Config::new(&args);
+        match result {
+            Ok(config) => {
+                    assert_eq!(config.query, "bar");
+                    assert_eq!(config.filename, "baz");
+            }
+            Err(err) => assert!(false, err)
+        }
+    }
+
+    #[test]
+    fn new_config_extra_arguments() {
+        let args = [String::from("foo"), String::from("bar"), String::from("baz"), String::from("qux")];
+        let result = Config::new(&args);
+        match result {
+            Ok(config) => {
+                    assert_eq!(config.query, "bar");
+                    assert_eq!(config.filename, "baz");
+            }
+            Err(err) => assert!(false, err)
+        }
+    }
+}
